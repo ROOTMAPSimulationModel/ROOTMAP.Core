584cfe06494b14a77077ce88fd41f560ecf3978c
Mike Airey <mike.s.airey@gmail.com>
2021-07-02 22:41:35 +1000

    Script bugfix.

--- core/version-stamp.ps1
+++ core/version-stamp.ps1
@@ -1,5 +1,5 @@
 # Run the copy of GitVersion installed in the packages directory to get GitVersion information for this project.
-$gitVersionExe = Get-ChildItem -Recurse ..\.. GitVersion.exe
+$gitVersionExe = (Get-ChildItem -Recurse ..\.. GitVersion.exe).FullName
 $gitVersionJson = & $gitVersionExe -output json
 
 Write-Host "GitVersion output:"
